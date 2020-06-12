924427405503ae75cc45df190e5320e3ecf50ce1
Surya Man Shrestha <surya1243@outlook.com>
2020-06-12 19:33:06 +0545

    Updates

---
+++ precache-manifest.87110f7d31ef052ec67a92754f4ac4c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4211794ddb096cae2ef8cfd0386880",
+    "url": "/Emoji.git/index.html"
+  },
+  {
+    "revision": "5a4870e0f8c8c1e569d4",
+    "url": "/Emoji.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3ccd507d904b7e54ff35",
+    "url": "/Emoji.git/static/js/2.4f3b5df3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Emoji.git/static/js/2.4f3b5df3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a4870e0f8c8c1e569d4",
+    "url": "/Emoji.git/static/js/main.b8e233bc.chunk.js"
+  },
+  {
+    "revision": "0289a6905e96e423204d",
+    "url": "/Emoji.git/static/js/runtime-main.d711cd90.js"
+  }
+]);
